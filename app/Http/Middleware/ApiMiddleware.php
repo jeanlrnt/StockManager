8845a4c71d3e622df898086d6078b4bff7f9dece
jeanl <jeanz.laurent@gmail.com>
2023-07-30 16:49:50 +0200

    add apiMiddleware

---
+++ app/Http/Middleware/ApiMiddleware.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Routing\ResponseFactory;
+use Symfony\Component\HttpFoundation\Response;
+
+class ApiMiddleware
+{
+    /**
+     * The Response Factory our app uses
+     *
+     * @var ResponseFactory
+     */
+    protected ResponseFactory $factory;
+
+    /**
+     * JsonMiddleware constructor.
+     *
+     * @param ResponseFactory $factory
+     */
+    public function __construct(ResponseFactory $factory)
+    {
+        $this->factory = $factory;
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param Closure $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $request->headers->set('Accept', 'application/json');
+
+        $response = $next($request);
+
+
+        if (!$response instanceof JsonResponse) {
+            $response = $this->factory->json(
+                $response->content(),
+                $response->status(),
+                $response->headers->all()
+            );
+        }
+
+        return $response;
+    }
+}

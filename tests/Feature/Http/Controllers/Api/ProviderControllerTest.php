f10177e83ba64ac0648640b20e54da8d66af9b75
jeanl <jeanz.laurent@gmail.com>
2023-08-01 11:11:50 +0200

    fix test
    Signed-off-by: jeanl <jeanz.laurent@gmail.com>

--- tests/Feature/Http/Controllers/Api/ProviderControllerTest.php
+++ tests/Feature/Http/Controllers/Api/ProviderControllerTest.php
@@ -2,9 +2,24 @@
 
 namespace Http\Controllers\Api;
 
+use App\Models\Provider;
+use App\Models\User;
+use Laravel\Sanctum\Sanctum;
 use tests\TestCase;
 
 class ProviderControllerTest extends TestCase
 {
+    public function test_index_returns_200_status_code_when_user_is_authorized(): void
+    {
+        Sanctum::actingAs(
+            User::factory()->create(),
+            ['viewAny']
+        );
 
+        Provider::factory()->count(5)->create();
+
+        $response = $this->get('/api/providers');
+
+        $response->assertStatus(200);
+    }
 }
